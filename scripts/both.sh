89384065f61c4ff122d595e096c397bd6ebc6af0
miermontoto <mier@mier.info>
2023-09-29 22:05:27 +0200

    mover y corregir scripts de ejecución de prueba

--- scripts/both.sh
+++ scripts/both.sh
@@ -1,10 +1,13 @@
-. ./values.sh
+#!/bin/bash
+. ./scripts/values.sh
 
-params="${xmin} ${xmax} ${ymin} ${maxiter} onlytimes py own sizes 256 512 1024"
+params="${xmin} ${xmax} ${ymin} ${maxiter} onlytimes py own sizes 256 512"
 
 # Secuencial
 export OMP_NUM_THREADS=1
 python Launcher.py $params
+
+echo
 
 # Paralelo
 unset OMP_NUM_THREADS
--- scripts/default.sh
+++ scripts/default.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #export LD_PRELOAD="$CUDADIR/lib64/libcudart.so $CUDADIR/lib64/libcublas.so"
-. ./values.sh
+. ./scripts/values.sh
 
 unset OMP_NUM_THREADS
 python Launcher.py $xmin $xmax $ymin $maxiter prof own methods all sizes 256 512 1024 onlytimes                                                                                                                                                                                                                                                                                                                                                                                                              #| column -t -s ';'
